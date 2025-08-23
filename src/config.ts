56a25a8af757feb15b4e00a26258ff0cf9de9541
Williamszz <2825096839@qq.com>
2025-08-23 23:18:04 +0800

    commit again

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "William",
-	bio: "长风破浪会有时，直挂云帆济沧海",
+	bio: "长风破浪会有时   直挂云帆济沧海",
 	links: [
 		{
 			name: "Twitter",
